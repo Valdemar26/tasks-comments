29f9f1af2138d5be32c924de3fc24d9a2f4ddc6a
Volodymyr Zaplatynskii <v.zaplatunskuj@crosssoft.de>
2019-10-07 15:22:27 +0300

    work with counting comments

--- src/app/main/components/comments/comments.component.ts
+++ src/app/main/components/comments/comments.component.ts
@@ -1,5 +1,6 @@
 import {Component, ElementRef, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
 import { DataService } from '../../../service/data.service';
+import {first} from 'rxjs/operators';
 
 @Component({
   selector: 'app-comments',
@@ -8,12 +9,10 @@ import { DataService } from '../../../service/data.service';
 })
 
 export class CommentsComponent implements OnInit, OnChanges {
-
-  @Input() item: any;
+  @Input() itemObj: any;
 
   comments = [];
   comment: string;
-  color: any;
 
   constructor(private dataService: DataService) { }
 
@@ -22,38 +21,30 @@ export class CommentsComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
-    if (this.item) {
-      this.comments = this.item.comments;
+    if (this.itemObj) {
+      this.comments = this.itemObj.item.comments;
     }
-    this.dataService.item$.subscribe(
-      data => data ? this.comments = [] : null
+    this.dataService.item$.pipe(first()).subscribe(
+      data =>  {
+        this.comments = data.deleteItem ? [] : null;
+        console.log(this.comments);
+      }
     );
   }
 
   getAllComments() {
-    this.dataService.getAllItems().subscribe(item => {
+    this.dataService.getAllItems().pipe(first()).subscribe(item => {
       this.comments = item.comment;
     });
   }
 
   setCommentByItemId(comment) {
-    this.dataService.setCommentByItemId(comment, this.item).subscribe( (data: any) => {
+    this.dataService.setCommentByItemId(comment, this.itemObj.item).pipe(first()).subscribe( (data: any) => {
       console.log('DATA: ', data.comments);
       this.comments = data.comments;
     });
+
     this.comment = '';
-  }
-
-  // getRandomColor() {
-  //   const color = Math.floor(0x1000000 * Math.random()).toString(16);
-  //   console.log('#' + ('000000' + color).slice(-6));
-  //   return this.color = '#' + ('000000' + color).slice(-6);
-  // }
-
-  getRandomColor(max) {
-    const color = '#' + ('000000' + Math.floor(0x1000000 * Math.random()).toString(16)).slice(-6);
-    console.log(color, max);
-    return this.color = color;
   }
 
 }
--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,14 +1,14 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
-import {Observable, Subject} from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { map, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
-  item$: Subject<boolean> = new Subject();
+  item$: Subject<any> = new Subject();
 
   private baseUrl = environment.baseUrl;
 
